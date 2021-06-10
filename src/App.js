e00b10565783a22afd7537f686f2df257072a9a7
rachel <rng@stuy.edu>
2021-06-09 23:15:17 -0400

    create and update time

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import React from 'react';
 import Header from "./component/header"
 import './App.css';
+import { Container, Box, Button } from '@material-ui/core';
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            created: new Date(),
+            updated: new Date() 
+        };
+    }
+
+    updateTime = () => {
+        console.log(this.state.created);
+        this.setState({
+            updated: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Updated at {this.state.updated.toLocaleTimeString()}.</h2>
+                <p>Created {this.state.created.toLocaleString()}.</p>
+                <Button variant="contained" color="primary" onClick={this.updateTime}>Update</Button>
+            </div>
+        );
+    }
+}
+
 
 function App() {
-  return (
-    <div className="App">
-    <Header/>
+    return (
+    <div>
+        <Header/>
+        <Container maxWidth="md">
+            <Box>
+                <Clock/>
+            </Box>
+        </Container>
     </div>
-  );
+    );
 }
 
 export default App;
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,3 +1,13 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+
 export default function Header(){
-    return <div>2021 IBM Accelerate Software Track Lab Boiler Plate</div>
+    return (
+    <Box component="div" bgcolor="primary.main" color="white" p="1.5em" mb="2em">
+        <Container maxWidth="md">
+            2021 IBM Accelerate Software Track Lab Boiler Plate
+        </Container>
+    </Box>
+    );
 }
