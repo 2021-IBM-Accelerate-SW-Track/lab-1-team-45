3595f1a1e99efb3f0ae4fb7a142df3a7d73a2562
consolelogg <78403820+consolelogg@users.noreply.github.com>
2021-06-16 21:42:26 -0400

    Adding css styles to header

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
+import './header.css'
 
 export default function Header(){
     return (
-    <Box component="div" bgcolor="primary.main" color="white" p="1.5em" mb="2em">
+    <Box className ='headerbox' component="div" p="1.5em" mb="2em">
         <Container maxWidth="md">
             Group 45 To-Do List    
         </Container>
